1a5faf74f9c59729da59b01747ec16f2ef1639c2
Colby Jensen <cj433@byu.edu>
2023-03-11 23:15:27 -0700

    Added the ability for the user to delete items from cart.

--- Mission09_cj433/Startup.cs
+++ Mission09_cj433/Startup.cs
@@ -38,8 +38,10 @@ namespace Mission09_cj433
             services.AddRazorPages();
 
             services.AddDistributedMemoryCache();
-
             services.AddSession();
+
+            services.AddScoped<Basket>(x => SessionBasket.GetBasket(x));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
